eb6fd12dc346ca4132e99078161d30bcb24097f7
Shota Oki <oki.08031367557@i.softbank.jp>
2023-10-12 22:42:03 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -47,6 +47,71 @@ function DynamicContents(parameter: { content: string }) {
   );
 }
 
+function DemoContents() {
+  const sceneLoader = new DirectSceneLoader(
+    "/iot-app-kit-extra-document/demo-content.json"
+  );
+  const controller = useOverrideTags({
+    // TwinMakerのタグをボタンに置き換える
+    壁のボタン: (replaceTag) =>
+      replaceTag.toButton
+        ?.create({
+          angle: 90,
+          content: "Close",
+          width: 0.7,
+          height: 0.24,
+          stateStyle: _ButtonStyle.Standard,
+        })
+        .onClickEvent(() => {
+          console.log("clicked: 閉じる");
+        }),
+    // TwinMakerのタグをボタンに置き換える
+    兎田ぺこら: (replaceTag) =>
+      replaceTag.toMMD
+        ?.create({
+          angle: 0,
+          scale: 0.08,
+          pmxPath:
+            "/iot-app-kit-extra-document/mmd/UsadaPekora/PMX/UsadaPekora.pmx",
+          useMotionList: {
+            dance:
+              "/iot-app-kit-extra-document/mmd/Alicia/MMD Motion/2分ループステップ1.vmd",
+          },
+        })
+        .onStateChangeEvent((mesh, model, state) => {
+          return ["dance"];
+        }),
+    // TwinMakerのタグをボタンに置き換える
+    壁の時計: (replaceTag) =>
+      replaceTag.toText
+        ?.create({
+          angle: 0,
+          content: "Time",
+        })
+        .onAnimating((text) => {
+          text.set({
+            content: new Date(new Date().getTime() + 9 * 60 * 60 * 1000)
+              .toISOString()
+              .split(".")[0],
+          });
+        }),
+    部屋: (replaceTag) =>
+      replaceTag.toGLTF?.create({
+        angle: 0,
+        modelPath:
+          "/iot-app-kit-extra-document/example/studio_apartment_vray_baked_textures_included.glb",
+      }),
+  });
+
+  return (
+    <SceneViewer
+      sceneComposerId={controller.composerId}
+      sceneLoader={sceneLoader}
+      activeCamera="Camera1"
+    />
+  );
+}
+
 function App() {
   const [ReplaceView, SetReplaceView] = useState<JSX.Element>(<div />);
 
@@ -65,6 +130,8 @@ function App() {
       response.text().then((content) => {
         if (content.includes("${0}")) {
           SetReplaceView(<DynamicContents content={content} />);
+        } else if (content.includes("${DEMO}")) {
+          SetReplaceView(<DemoContents />);
         }
       });
     });
